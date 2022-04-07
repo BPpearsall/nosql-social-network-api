c71ce362ac0f68667309739b9d320cc92b40d761
Lyere <BPpearsall@gmail.com>
2022-04-07 15:54:18 -0400

    added correct file/folder strucutre and constructed models and usercontrollers

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: { 
+            type: String, 
+            required: true, 
+            unique: true, },
+        email: { 
+            type: String, 
+            required: true, 
+            unique: true, 
+            match: [regularExpression, "Must match email address"],
+        },
+        thoughts: [{ type: Schema.Types.ObjectId, ref: 'thought'}],
+        friends: [{ type: Schema.Types.ObjectId, ref: 'user'}], 
+    },
+    {
+        toJSON: {
+            virtual: true,
+        },
+        id: false
+    }
+);
+
+userSchema
+    .virtual('friendCount')
+    .get(function() {
+        return this.friends.length;
+    });
+
+const User = model('user', userSchema);
+
+module.exports = User;
